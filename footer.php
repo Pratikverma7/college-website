a68cdd0b88db14e7de2b44ca590f94aed43c2e08
WebCluster Private Limited <83221578+webclusterdelhi@users.noreply.github.com>
2021-04-26 20:39:01 +0530

    commit

---
+++ footer.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webcluster.in Footer</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="footerstyle.css">
+
+</head>
+<body>
+
+    <footer>
+        <div class="container">
+            <div class="sec aboutus">
+            <img style="max-width:200px" src="logo.png"></img>
+              <br><br><br>
+                <p>CIC is a new innitiative by delhi university to make students creative and innovative. It have limited courses & seats
+                </p> <br><br>
+                
+            </div>
+
+            <div class="sec quicklinks">
+               <h2>Study @ CIC</h2>
+               <ul>
+                   <li><a href="">B.Tech</a></li>
+                   <li><a href="">B.A Honours</a></li>
+                   <li><a href="">M.Sc</a></li> 
+               </ul>
+            </div>
+
+
+            <div class="sec quicklinks">
+               <h2>About CIC</h2>
+               <ul>
+                   <li><a href="">Objective</a></li>
+                   <li><a href="">Facility</a></li>
+                   <li><a href="">Governance</a></li>
+                   <li><a href="">Collaborations</a></li>
+               </ul>
+            </div>
+
+            <div class="sec contact">
+                <h2>Contact Us</h2>
+                <ul class="info">
+                    <li> 
+                        <span><i class="fa fa-map-marker" area-hidden="true"></i></span><span>North Campus<br>
+                         University Campus, Delhi-110004</span>
+                   </li>
+                   <li> 
+                        <span><i class="fa fa-phone" area-hidden="true"></i></span>
+                       <p><a href="tel:+1234567890">XXX-XXX-XXXX</a><br>
+                          <a href="tel:+1234567890">XXX-XXX-XXXX</a>
+                      </p>
+                   </li>
+                   <li> 
+                        <span><i class="fa fa-envelope" area-hidden="true"></i></span>
+                       <p><a href="mailto:info@ducic.com">info@ducic.com</a><br></p>
+                   </li>
+                   <li> 
+                        <span><i class="fa fa-user" area-hidden="true"></i></span><span><a href="">Log-In</a></span>
+                   </li>
+                </ul>
+
+                 
+            </div>
+        </div>
+    </footer>
+
+    <div class="copyrightText">
+        <p>Copyright &copy 2021 <a href="https://webcluster.in">WebCluster Private Limited.</a> All Rights Reserved.</p>
+    </div>
+ 
+</body>
+</html>
